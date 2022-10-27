3361b3e073d9db3f04398c9dba9ebe576a998624
Ashutosh Bharti <ashutosh.bharti5247@gmail.com>
2022-10-27 22:16:40 +0530

    collection id changed

--- src/networking/FirebaseAPI.controller.js
+++ src/networking/FirebaseAPI.controller.js
@@ -2,7 +2,7 @@ import firestore from '@react-native-firebase/firestore';
 
 const connectToFirestoreforIsPaid = async () => {
   try {
-    const collection = await firestore().collection('check').doc('11').get();
+    const collection = await firestore().collection('sczksn').doc('vrPBpXY8JRJdS7ZdJjXs').get();
     const val = collection.data();
     console.log('val', val);
     return val.paid;
