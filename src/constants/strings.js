6b2241b2191fa19978789efd353301cbc9a79cd6
Shubhi Singh <shubhisingh1506@gmail.com>
2022-11-05 13:33:43 +0530

    Ui fix

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -8,9 +8,14 @@ export const STRINGS = {
     RESEND_OTP: 'Resend OTP',
     TYPE_OF_CENTER: 'Type Of Center',
     CLASS_FREQUENCY: 'Class Frequency',
+    DURATION :"Duration Of Class" ,
+    PLACE_USED : "Place Used" ,
+    REGULAR_STUDENTS : "No. Of Regular Students" ,
+    ENROLLED_STUDENTS : " No. Of Enrolled Students"
   },
   SIGNUP: {
     DURATION_OF_CLASS: 'Duration Of Class',
     DATE_OF_EST: 'Date Of Establishment',
+
   },
 };
