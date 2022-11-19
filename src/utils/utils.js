39c32b08ecfbd82b4b746449a751075950eb38b0
Ashutosh Bharti <ashutosh.bharti5247@gmail.com>
2022-11-19 15:37:49 +0530

    minor change

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,7 +7,7 @@ import I18n from 'i18n-js';
 export const ChangeLanguageAndReboot = (lang, t) => {
   return Alert.alert(
     `${t('LANGUAGE_CHANGE')}`,
-    `${t('APP_LANGUAGE_CHANGED')}.\n${t('APP_RESTART_REQUEST')}`,
+    `${t('APP_RESTART_REQUEST')}`,
     [
       {
         text: t('CANCEL'),
